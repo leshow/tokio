2dd316b5b2436c3e29ca9246b1cda37ea1143409
Evan Cameron <cameron.evan@gmail.com>
2020-09-25 11:29:22 -0400

    test: show failing udp split

--- tokio/tests/udp.rs
+++ tokio/tests/udp.rs
@@ -1,8 +1,8 @@
 #![warn(rust_2018_idioms)]
 #![cfg(feature = "full")]
 
-use std::sync::Arc;
-use tokio::net::UdpSocket;
+use std::{net::SocketAddr, sync::Arc};
+use tokio::{net::UdpSocket, sync::mpsc};
 
 const MSG: &[u8] = b"hello";
 const MSG_LEN: usize = MSG.len();
@@ -52,6 +52,37 @@ async fn split() -> std::io::Result<()> {
     });
     let mut recv_buf = [0u8; 32];
     let (len, _) = r.recv_from(&mut recv_buf[..]).await?;
+    assert_eq!(&recv_buf[..len], MSG);
+    Ok(())
+}
+
+#[tokio::test]
+async fn split_chan() -> std::io::Result<()> {
+    // setup UdpSocket that will echo all sent items
+    let socket = UdpSocket::bind("127.0.0.1:0").await?;
+    let addr = socket.local_addr().unwrap();
+    let s = Arc::new(socket);
+    let r = s.clone();
+
+    let (tx, mut rx) = mpsc::channel::<(Vec<u8>, SocketAddr)>(1_000);
+    tokio::spawn(async move {
+        while let Some((bytes, addr)) = rx.recv().await {
+            s.send_to(&bytes, &addr).await.unwrap();
+        }
+    });
+
+    tokio::spawn(async move {
+        let mut buf = [0u8; 32];
+        loop {
+            let (len, addr) = r.recv_from(&mut buf).await.unwrap();
+            tx.send((buf[..len].to_vec(), addr)).await.unwrap();
+        }
+    });
+    // test that we can send a value and get back some response
+    let sender = UdpSocket::bind("127.0.0.1:0").await?;
+    sender.send_to(MSG, addr).await?;
+    let mut recv_buf = [0u8; 32];
+    let (len, _) = sender.recv_from(&mut recv_buf).await?;
     assert_eq!(&recv_buf[..len], MSG);
     Ok(())
 }
