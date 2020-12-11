5bad8413ad2c66085f682373a4f3272e58ef43aa
github-action-benchmark <github@users.noreply.github.com>
2020-12-11 17:22:44 +0000

    add rt_multi_threaded (cargo) benchmark result for 69e62ef89e481e0fb29ce3ef4ddce1eea4114000

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1607707353568,
+  "lastUpdate": 1607707364585,
   "repoUrl": "https://github.com/leshow/tokio",
   "entries": {
     "sync_rwlock": [
@@ -53,6 +53,56 @@ window.BENCHMARK_DATA = {
             "name": "read_uncontended",
             "value": 493,
             "range": "± 9",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ],
+    "rt_multi_threaded": [
+      {
+        "commit": {
+          "author": {
+            "email": "alice@ryhl.io",
+            "name": "Alice Ryhl",
+            "username": "Darksonn"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "69e62ef89e481e0fb29ce3ef4ddce1eea4114000",
+          "message": "sync: make TryAcquireError public (#3250)\n\nThe [`Semaphore::try_acquire`][1] method currently returns a private error type.\r\n\r\n[1]: https://docs.rs/tokio/0.3/tokio/sync/struct.Semaphore.html#method.try_acquire",
+          "timestamp": "2020-12-10T19:56:05-08:00",
+          "tree_id": "0784747565f6583a726c85dfedcd0527d8373cc6",
+          "url": "https://github.com/leshow/tokio/commit/69e62ef89e481e0fb29ce3ef4ddce1eea4114000"
+        },
+        "date": 1607707363586,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "chained_spawn",
+            "value": 157636,
+            "range": "± 3508",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ping_pong",
+            "value": 622756,
+            "range": "± 20095",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spawn_many",
+            "value": 4631154,
+            "range": "± 707799",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "yield_many",
+            "value": 18330893,
+            "range": "± 2212937",
             "unit": "ns/iter"
           }
         ]
